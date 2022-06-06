48999df2b92f9d5528c262adcb43e0c37f7103f3
camotito0 <cecilianallerena@gmail.com>
2022-06-06 15:44:10 -0500

    Añadiendo función filterData en el archivo data.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,29 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
+export const sortData = (data, sortBy, sortOrder) => {
+  //sortData(data, sortBy, sortOrder)
   return 'example';
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+
+//filtrar datos por tipo, generación y egg
+const types = [ 'poison', 'fire', 'normal', "fire", 'flying', 'dragon', 
+'water', 'dark', 'ice', 'steel', 'bug', 'psychic', 'steel',
+'ground', 'electric', 'fighting', 'rock','ghost', 'dragon','fairy']
+const generations = ['kanto', 'johto']
+const eggs = ['2 km', '5 km', '7 km', '10 km', 'not in eggs']
+
+export const filterData = (data, condition) => {
+  if(types.includes(condition)) {
+    var result = data.filter( pokemon => pokemon.type.includes(condition) )
+    return result 
+  } else if (generations.includes(condition)) {
+    var result = data.filter( pokemon => pokemon.generation.name.includes(condition) )
+    return result
+  } else if (eggs.includes(condition)) {
+    var result = data.filter( pokemon => pokemon.egg.includes(condition) )
+    return result
+  } else {
+    return 'error'
+  }
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+import {filterData, sortData} from './data.js';
 
-console.log(example, data);
+
+console.log(filterData(data.pokemon, '7 km'))
+console.log(sortData(data.pokemon, filterData, 'asc'))
+
+//console.log(example, data);
