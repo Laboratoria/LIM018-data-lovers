a55837b9cc8d6b59916a11422333a3a59cbbf3d2
Bellasacc <bellasacc10@gmail.com>
2022-06-06 16:18:47 -0500

    ordenando data por nombre y cantidad de debilidades del pokemon

--- src/data.js
+++ src/data.js
@@ -1,7 +1,21 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+export const sortData = (data,sortBy,sortOrder) => {
+  let arrayOrder;
+    arrayOrder = data.pokemon.sort((a, b)=> {
+        if (a.name.toLowerCase() > b.name.toLowerCase()) return 1;
+        if (a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        return 0;
+    })
+    if(sortBy === 'name' && sortOrder === 'asc'){
+      return arrayOrder
+    }else if(sortBy==='name' && sortOrder === 'desc'){
+      return arrayOrder.reverse()
+    }else if(sortBy === 'weaknesses' && sortOrder === 'asc'){
+      return data.pokemon.sort((a, b)=> a.weaknesses.length - b.weaknesses.length)
+    }else if(sortBy==='weaknesses' && sortOrder === 'desc'){
+      return  data.pokemon.sort((a, b)=> b.weaknesses.length - a.weaknesses.length)
+    } 
 };
 
 export const anotherExample = () => {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import { example } from './data.js';
+import { sortData } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data);
 
 //data.pokemon;
 //section
@@ -66,3 +66,6 @@ mapedData.forEach(element => {
 
     //console.log(element);
 });
+
+//ordenando de forma ascedente por nombre de pokemon, por el momento se muestra en consola
+console.log(sortData(data,'name','asc'))
