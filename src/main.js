b5515b6e8e56cb9aef6c5847dc3e0aa7aa50b25b
Deleina llamocca <marta.deleina@gmail.com>
2022-06-07 09:30:14 -0500

    merge main.js

--- src/main.js
+++ src/main.js
@@ -6,14 +6,16 @@ import data from './data/pokemon/pokemon.js';
 console.log(data);
 
 //data.pokemon;
+
 //section
 const infoPokemonSection = document.getElementById('info-pokemon-section')
 
-const datosPokemon=data.pokemon;
+const dataPokemon=data.pokemon;
 
 //map solo para mapear los datos  pedidos en la historia
-const mapedData = datosPokemon.map((itemPokemon)=>{
+const mapedData = dataPokemon.map((itemPokemon)=>{
  return {
+    num:itemPokemon.num,
     name:itemPokemon.name,
     img:itemPokemon.img,
     heigth:itemPokemon.size.height,
@@ -26,19 +28,12 @@ const mapedData = datosPokemon.map((itemPokemon)=>{
 mapedData.forEach(element => {
     //divcard
     const divCardPokemon=document.createElement('div');
-    //class title-pokemon
+
+    //class title-pokemon    
     divCardPokemon.classList.add('title-pokemon');
     infoPokemonSection.appendChild(divCardPokemon);
 
-    //h2title
-    const h2title = document.createElement('h2');
-    divCardPokemon.appendChild(h2title);
-
-    //titulo pokemon
-    const titlePokemon=document.createTextNode(element.name)
-    h2title.appendChild(titlePokemon);
-
-    //div-creado
+    //div-creado-para img
     const createdDiv = document.createElement('div');
     createdDiv.classList.add('container');
     divCardPokemon.appendChild(createdDiv);
@@ -48,22 +43,37 @@ mapedData.forEach(element => {
     createImg.src = element.img
     createdDiv.appendChild(createImg);
 
-    //parrafo-creado-para heigth-pekemon
-    const createdParagraph = document.createElement('p');
-    divCardPokemon.appendChild(createdParagraph);
+    //h2title
+    const h2title = document.createElement('h2');
+    divCardPokemon.appendChild(h2title);
 
-    //pokemon-heigth
-    const heigthPokemon=document.createTextNode(element.heigth);
-    createdParagraph.appendChild(heigthPokemon);
-    
-    //parrafo-creado-para weigth-pokemon
+    //titulo pokemon
+    const titlePokemon = document.createTextNode(element.name)
+    h2title.appendChild(titlePokemon);
+
+    //parrafo-creado-para numero de pokemon
     const createdParagraph1 = document.createElement('p');
     divCardPokemon.appendChild(createdParagraph1);
 
+    //pokemon-numero
+    const numPokemon = document.createTextNode(element.num);
+    createdParagraph1.appendChild(numPokemon);
+
+    //parrafo-creado-para heigth-pokemon
+    const createdParagraph2 = document.createElement('p');
+    divCardPokemon.appendChild(createdParagraph2);
+
+    //pokemon-heigth
+    const heigthPokemon=document.createTextNode(element.heigth);
+    createdParagraph2.appendChild(heigthPokemon);
+    
+    //parrafo-creado-para weigth-pokemon
+    const createdParagraph3 = document.createElement('p');
+    divCardPokemon.appendChild(createdParagraph3);
+
     //pokemon-weigth
     const weigthPokemon = document.createTextNode(element.weight);
-    createdParagraph1.appendChild(weigthPokemon);
-
+    createdParagraph3.appendChild(weigthPokemon);
     //console.log(element);
 });
 
