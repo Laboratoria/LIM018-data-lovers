f1780df70df0559dc6e0c3e1aee2f943bf91b80d
Angelica Lopez <angelica.lopezcap@gmail.com>
2022-06-14 11:20:26 -0500

    agregando Fetch para leer la data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/ghibli/ghibli.js';
 
-console.log(example, data);
+//console.log(fetch('https://ghibliapi.herokuapp.com/films'));
+fetch('./data/ghibli/ghibli.json/')
+ .then(response => {
+    if (response.ok) {
+    console.log('success')
+ }else{
+    console.log('error')
+ }
+ response.json()
+ })
+ .then(data => console.log(data));
+//console.log(example, data);
