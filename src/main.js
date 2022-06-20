245779abc244abf26f8ab83c48d87c92f45cd569
Denimarx <denimar2022@gmail.comm>
2022-06-20 17:49:15 -0500

    comentario

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
-import { filterDataDirector} from './data.js';
+import { filterByDirector} from './data.js';
 //import jsondata from './data/ghibli/ghibli.js';
+
+
+
+
+  fetch ('./data/ghibli/ghibli.json')
+   .then(response => response.json())
+   .then(data => { 
+    let containerFilms =document.getElementById('containerFilms');
+
+    data.films.forEach(film => {
+  
+      containerFilms.innerHTML += `<img src="${film.poster}" alt="imagen" >`;
+    });
+    
+    console.log(data)
+   }
+   )
+
+   .catch(err =>console.log (err))
+
 
 
 
