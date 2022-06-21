61b0491722c77def43b24302b402bc3586d02ad9
@StefaniPenaranda <pstefanipm@gmail.com>
2022-06-21 00:51:19 -0500

    ver peliculas y css

--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
-import { filterDataDirector} from './data.js';
-//import jsondata from './data/ghibli/ghibli.js';
+import { filterByDirector} from './data.js';
 
 
+fetch ('./data/ghibli/ghibli.json')
+.then(response => response.json())
+.then(data => {
+ let containerFilms =document.getElementById('containerFilms');
+
+ data.films.forEach(film => {
+
+   containerFilms.innerHTML += `<img src="${film.poster}" alt="imagen" >`;
+ });
+
+ console.log(data)
+}
+)
+.catch(err =>console.log (err))
 
 
+const portada = document.querySelector('.header-h');
+const botonPeliculas = document.querySelector('.lista-peliculas');
+const containerFilms = document.querySelector('.contenedor-paginas');
 
-
-
-
-
-
-
-
+const verpeliculas = () =>{
+  portada.style.display = "none";
+  containerFilms.style.display = "block";
+}
+botonPeliculas.addEventListener('click', verpeliculas);
 
 
