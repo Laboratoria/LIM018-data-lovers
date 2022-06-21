5537f2f95ed81b87d6217cd1b2495a72eba1cd04
Bellasacc <bellasacc10@gmail.com>
2022-06-20 19:23:36 -0500

    testeando sortData

--- test/pokemon.js
+++ test/pokemon.js
@@ -293,7 +293,7 @@ export const filterByEgg = [
     },
 ]
 
-
+////resultado para testear mapedData obteniendo informacion basica de los pokemones
 export const basicInfo = [
   {
     "num": "001",
@@ -355,5 +355,582 @@ export const basicInfo = [
     "type": [
       "grass"
     ],
+  },
+]
+//resultado para testear sortData respecto al ordenado por nombre de forma ascendente
+export const orderByNameAsc = [
+  {
+    "num": "153",
+    "name": "bayleef",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/153.png",
+    "size": {
+      "height": "1.19 m",
+      "weight": "15.8 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "not in eggs",
+  },
+  {
+    "num": "001",
+    "name": "bulbasaur",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto" 
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "size": {
+      "height": "0.71 m",
+      "weight": "6.9 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "flying",
+      "psychic"
+    ],
+    "egg": "2 km",
+  },
+  {
+      "num": "004",
+      "name": "charmander",
+      "generation": {
+        "num": "generation i",
+        "name": "kanto"
+      },
+      "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+      "size": {
+        "height": "0.61 m",
+        "weight": "8.5 kg"
+      },
+      "pokemon-rarity": "normal",
+      "type": [
+        "fire"
+      ],
+      "weaknesses": [
+        "water",
+        "ground",
+        "rock"
+      ],
+      "egg": "2 km",
+    },
+    {
+    "num": "005",
+    "name": "charmeleon",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/005.png",
+    "size": {
+      "height": "1.09 m",
+      "weight": "19.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "not in eggs",
+    },
+    {
+      "num": "152",
+      "name": "chikorita",
+      "generation": {
+        "num": "generation ii",
+        "name": "johto"
+      },
+      "img": "https://www.serebii.net/pokemongo/pokemon/152.png",
+      "size": {
+        "height": "0.89 m",
+        "weight": "6.4 kg"
+      },
+      "pokemon-rarity": "normal",
+      "type": [
+        "grass"
+      ],
+      "weaknesses": [
+        "fire",
+        "ice",
+        "poison",
+        "flying"
+      ],
+      "egg": "2 km",
+    },
+    {
+      "num": "007",
+      "name": "squirtle",
+      "generation": {
+        "num": "generation i",
+        "name": "kanto"
+      },
+      "img": "https://www.serebii.net/pokemongo/pokemon/007.png",
+      "size": {
+        "height": "0.51 m",
+        "weight": "9.0 kg"
+      },
+      "pokemon-rarity": "normal",
+      "type": [
+        "water"
+      ],
+      "weaknesses": [
+        "electric",
+        "grass"
+      ],
+      "egg": "2 km",
+    },
+]
+//resultado para testear sortData respecto al ordenado por nombre de forma descendente
+export const orderByNameDesc = [
+  {
+    "num": "007",
+    "name": "squirtle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/007.png",
+    "size": {
+      "height": "0.51 m",
+      "weight": "9.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "152",
+    "name": "chikorita",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/152.png",
+    "size": {
+      "height": "0.89 m",
+      "weight": "6.4 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "005",
+    "name": "charmeleon",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/005.png",
+    "size": {
+      "height": "1.09 m",
+      "weight": "19.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "not in eggs",
+  },
+  {
+    "num": "004",
+    "name": "charmander",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+    "size": {
+      "height": "0.61 m",
+      "weight": "8.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "2 km",
+  },
+  
+  {
+    "num": "001",
+    "name": "bulbasaur",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto" 
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "size": {
+      "height": "0.71 m",
+      "weight": "6.9 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "flying",
+      "psychic"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "153",
+    "name": "bayleef",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/153.png",
+    "size": {
+      "height": "1.19 m",
+      "weight": "15.8 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "not in eggs",
+  },
+]
+//resultado para testear sortData respecto al ordenado por cantidad de debilidades de forma ascendente
+export const orderByLenghtWeaknessesAsc = [
+  {
+    "num": "007",
+    "name": "squirtle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/007.png",
+    "size": {
+      "height": "0.51 m",
+      "weight": "9.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "004",
+    "name": "charmander",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+    "size": {
+      "height": "0.61 m",
+      "weight": "8.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "005",
+    "name": "charmeleon",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/005.png",
+    "size": {
+      "height": "1.09 m",
+      "weight": "19.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "not in eggs",
+  },
+  {
+    "num": "001",
+    "name": "bulbasaur",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto" 
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "size": {
+      "height": "0.71 m",
+      "weight": "6.9 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "flying",
+      "psychic"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "152",
+    "name": "chikorita",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/152.png",
+    "size": {
+      "height": "0.89 m",
+      "weight": "6.4 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "153",
+    "name": "bayleef",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/153.png",
+    "size": {
+      "height": "1.19 m",
+      "weight": "15.8 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "not in eggs",
+  },
+]
+//resultado para testear sortData respecto al ordenado por cantidad de debilidades de forma descendente
+export const orderByLenghtWeaknessesDesc = [
+  {
+    "num": "001",
+    "name": "bulbasaur",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto" 
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/001.png",
+    "size": {
+      "height": "0.71 m",
+      "weight": "6.9 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass",
+      "poison"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "flying",
+      "psychic"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "152",
+    "name": "chikorita",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/152.png",
+    "size": {
+      "height": "0.89 m",
+      "weight": "6.4 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "153",
+    "name": "bayleef",
+    "generation": {
+      "num": "generation ii",
+      "name": "johto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/153.png",
+    "size": {
+      "height": "1.19 m",
+      "weight": "15.8 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "grass"
+    ],
+    "weaknesses": [
+      "fire",
+      "ice",
+      "poison",
+      "flying"
+    ],
+    "egg": "not in eggs",
+  }, 
+  {
+    "num": "004",
+    "name": "charmander",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/004.png",
+    "size": {
+      "height": "0.61 m",
+      "weight": "8.5 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "2 km",
+  },
+  {
+    "num": "005",
+    "name": "charmeleon",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/005.png",
+    "size": {
+      "height": "1.09 m",
+      "weight": "19.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "fire"
+    ],
+    "weaknesses": [
+      "water",
+      "ground",
+      "rock"
+    ],
+    "egg": "not in eggs",
+  },
+  {
+    "num": "007",
+    "name": "squirtle",
+    "generation": {
+      "num": "generation i",
+      "name": "kanto"
+    },
+    "img": "https://www.serebii.net/pokemongo/pokemon/007.png",
+    "size": {
+      "height": "0.51 m",
+      "weight": "9.0 kg"
+    },
+    "pokemon-rarity": "normal",
+    "type": [
+      "water"
+    ],
+    "weaknesses": [
+      "electric",
+      "grass"
+    ],
+    "egg": "2 km",
   },
 ]
