55232033dd300fdf0a04ad746907f5f1d6a46773
Denimarx <denimar2022@gmail.comm>
2022-06-22 11:35:04 -0500

    sincronizado

--- src/data.js
+++ src/data.js
@@ -2,7 +2,6 @@
 //movie es elparametro q voy a iterar
 //FILTAR POR DIRECTOR
 
-
 export function filterByDirector (data, filtersDirector) {
   return data.filter(films => films.director == filtersDirector);
 }
