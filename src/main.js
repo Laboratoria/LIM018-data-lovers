b4c0e631e5b83764c34e8af5f5347889f787d786
Denimarx <denimar2022@gmail.comm>
2022-06-28 13:31:25 -0500

    avance2

--- src/main.js
+++ src/main.js
@@ -148,3 +148,34 @@ ordersAnRe.addEventListener('change', (event) =>{
     </div> ` ;
   })
 })
+
+//**********CRRUSEL************************
+/*const imgcarrusel = ['/src/imagenes/img1.jpg', '/src/imagenes/img2.jpg', '/src/imagenes/img3.jpg', '/src/imagenes/img4.jpg'],
+     cont = 0;
+    
+function carrusel(contenedor){
+  contenedor.addEventListener('click', e => {
+    let atras = contenedor.querySelector('.atras');
+        adelante = contenedor.querySelector('.adelante');
+        img = contenedor.querySelector('.imgcarrusel');
+        tgt = e.target;
+
+    if(tgt == atras){
+      if(cont > 0){
+        img.src = imagenes[cont - 1];
+        cont--;
+      } else {
+        img.src = imagenes[imagenes.length - 1];
+        cont = imagenes.length - 1;
+      }
+    } else if(tgt == adelante){
+      if(cont < imagenes.length - 1){
+        img.src = imagenes[cont + 1];
+        cont++;
+      } else {
+        img.src = imagenes[0];
+        cont = 0;
+      }
+  }}
+)
+}*/
