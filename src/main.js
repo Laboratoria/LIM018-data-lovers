f916c3070d74ff92482e2ad0a8dc34ae3862a080
Denimarx <denimar2022@gmail.comm>
2022-06-28 19:31:29 -0500

    intro

--- src/main.js
+++ src/main.js
@@ -10,13 +10,34 @@ const btnpeliculas = document.getElementById("listaPeliculas");
 const header = document.getElementById('header');
 const contenedorPaginas = document.getElementById('contenedorPaginas');
 const btnhome = document.getElementById("home");
-const containerhome = document.getElementById('containerhome')
+const containerFilms = document.getElementById('containerFilms')
+
+
+
+
+header.className = "enabled";
 
 btnpeliculas.addEventListener("click", () => {
   header.className = "disabled";
   contenedorPaginas.className = "enabled";
   const videoportada = document.getElementById('videoportada');
   videoportada.pause();
+  
+  fetch ('./data/ghibli/ghibli.json')
+ .then(response => response.json())
+ .then(data => {
+ let containerFilms =document.getElementById('containerFilms');
+ data.films.forEach(film => {
+   containerFilms.innerHTML +=  `<div class = "subContenedorDirector">
+   <img class="imgDirector"src="${film.poster}" alt="imagen" >  <div class = "directorDescripcion">
+   <p class="titleDirector"> ${film.title}</p>
+   <p class="parrafoDirector">Año:"${film.release_date}"</p>
+   <p class="parrafoDirector">Director:"${film.director}"</p>
+   <p class="parrafoDirector">Productor: "${film.producer}"</p>
+   </div>
+   </div> ` ;
+ });
+}).catch(err =>console.log (err))
 });
 
 btnhome.addEventListener("click", () => {
@@ -54,17 +75,10 @@ btnhome.addEventListener("click", () => {
 
 //******************************************************************************
 //llamar data y mostrar catalogo
-const containerFilms = document.getElementById('containerFilms');
 
-fetch ('./data/ghibli/ghibli.json')
-.then(response => response.json())
-.then(data => {
- let containerFilms =document.getElementById('.containerFilms');
- data.films.forEach(film => {
-   containerFilms.innerHTML += `<img src="${film.poster}" alt="imagen">`;
- });
-})
-.catch(err =>console.log (err))
+
+
+
 
 //***********************************************************
 const filtersDirector = document.getElementById("filtersDirector");
@@ -72,7 +86,6 @@ const filtersProducer = document.getElementById("filtersProducer");
 //filtar data por director****************************************
 filtersDirector.addEventListener("change", () => {
   const myMovies =orderAz(filterByDirector(data.films,filtersDirector.value))
-  containerhome.className = "disabled";
   containerFilms.innerHTML = "";
   myMovies.forEach(film => {
     containerFilms.innerHTML += `<div class = "subContenedorDirector">
@@ -87,7 +100,6 @@ filtersDirector.addEventListener("change", () => {
 //filtar data por productor*****************************************
 filtersProducer.addEventListener("change", () => {
   const myMoviesPro =(filterByProducer(data.films,filtersProducer.value))
-  containerhome.className = "disabled";
   containerFilms.innerHTML = "";
   myMoviesPro.forEach(film => {
     containerFilms.innerHTML += `<div class = "subContenedorDirector">
@@ -112,7 +124,6 @@ ordersAz.addEventListener('change', (event) =>{
   else {
     ordenar= orderZa(data.films)
   }
-  containerhome.className = "disabled";
   containerFilms.innerHTML = "";
   ordenar.forEach(film=>{
     containerFilms.innerHTML += `<div class = "subContenedorDirector">
@@ -135,7 +146,6 @@ ordersAnRe.addEventListener('change', (event) =>{
   else {
     ordenarAsc= sortByDesc(data.films)
   }
-  containerhome.className = "disabled";
   containerFilms.innerHTML = "";
   ordenarAsc.forEach(film=>{
     containerFilms.innerHTML += `<div class = "subContenedorDirector">
