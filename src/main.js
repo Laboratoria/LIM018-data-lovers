aebbf5d0d98a7bbbd5129a851f5edbbb7769da08
Roxann <lopezbedonroxana@gmail.com>
2022-06-30 22:10:03 -0500

    especi

--- src/main.js
+++ src/main.js
@@ -1,57 +1,33 @@
 import data from './data/rickandmorty/rickandmorty.js';
 
-const buscador = document.getElementById("buscador");
-const selectPersonajes = document.getElementById("selectPersonajes");
-const selectEspecies = document.getElementById("selectEspecie");
-// const content = document.getElementById("content")
-const personajes = data.results;
+const buscador = document.getElementById("#buscador")
+const content = document.getElementById("content")
+const selectPersonajes = document.getElementById("selectPersonajes")
 
-//vota la data de los humanos
-// let todoshumanos=document.getElementById("todoshumanos");
-// for ( let i = 0 ; i < personajes.length ; i++){
-//     let especies =personajes[i].species;
-//         if (especies === 'Human'){
-//           const nodeEspecie = document.createElement("div");
-//           // nodeEspecie.innerHTML=personajes[i].name
-//           nodeEspecie.innerHTML =`<img src="${personajes[i].image}"/> <p>${personajes[i].name}</p>`;
+const personajes = data.results
 
-//           todoshumanos.appendChild(nodeEspecie)
-                  
-//         }
-  
-//   }
-  // for ( let i = 0 ; i < personajes.length ; i++){
-  //   let especies =personajes[i].species;
-  //   console.log(personajes[i].species);
-  // }
-
-// ----------------------------------------------------------------
 // change nos regala informacion
 // cuando sucede algo
 // y es gratis.......
 // te la manda en forma de regalito, como argumento!!!!
 // el regalo es :ooooooo
 // la informacion de lo que suecede en el evento
-
-
 selectPersonajes.addEventListener("change", function (event) {
-const opcionSeleccionada = event.target.value
-if (opcionSeleccionada == "ah") {
+    const opcionSeleccionada = event.target.value
 
-//  content.innerHTML = ""
-   nodeEspecie.innerHTML=personajes[i].name
-   opcionSeleccionada.appendChild(selectPersonajes)
+    if (opcionSeleccionada == "ah") {
 
-//         // para recorrer arrays
-   personajes.forEach(function (personaje){
+        content.innerHTML = ""
+
+        // para recorrer arrays
+        personajes.forEach(function (personaje){
             
-personaje.name
+            //personaje.name
 
- let html = `<div><h3>${personaje.name}</h3><img src="${personaje.image}" /></div>`
- content.insertAdjacentHTML("beforeend", html)
-
-            
+            let html = `<div><h3>${personaje.name}</h3><img src="${personaje.image}" /></div>`
+            content.insertAdjacentHTML("beforeend", html)
         })
     }
 
 })
+
